42bbcb94ef4fafbcdf4869907e2c48c33ff22cf5
Chase <chambers.chase@gmail.com>
2023-04-26 21:23:55 -0400

    Not Dry version of code - Both cards showing

--- script.js
+++ script.js
@@ -1,46 +1,55 @@
 let newDeckUrl = 'https://deckofcardsapi.com/api/deck/new/';
 let shuffleDeckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
-let drawCards = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2';
+let drawCards = 'https://deckofcardsapi.com/api/deck/new/draw/?count=4';
 // let returnCards = `https://deckofcardsapi.com/api/deck/${deckId}/return/`
 let deckId = 1;
 let playerDeck = document.getElementById('player-deck');
 
 let playerCards = document.getElementById('player-cards');
+let opponentCards = document.getElementById('oppo-cards');
 
 // GET A DECK AND DRAW 1 CARD FUNCTION
-function drawSomeCards() {
+function drawPlayerCards() {
 return fetch(drawCards)
     .then((response) => {
         return response.json();
     })
         .then((data) => {
-            console.log(data);
             deckId = data.deck_id;
-
+            console.log(data)
+        
+        // function displayPlayerCardsInHtml(cards){
             let i = 0;
             for (i=0; i<2; i++) {
                 let cardImgElement = document.createElement('img');
                 cardImgElement.src= data.cards[i].image;
                 playerCards.appendChild(cardImgElement);
-        }
-        })
-    };
-//     var cards = [
-//         {
-//             "image" : "urlgoeshere"
-//         }
-//     ];
-// function displayCardsInHtml(cards){
-//     for(...){
-//     let cardImgElement = document.createElement('img');
-//                 cardImgElement.src= cards[i].image;
-//                 playerCards.appendChild(cardImgElement);
-//     }
-// }
-drawSomeCards()
+            }
+    });
+};
+
+function displayPlayerCardsInHtml(cards){
+    let i = 0;
+    for (i=0; i<2; i++) {
+        let cardImgElement = document.createElement('img');
+        cardImgElement.src= data.cards[i].image;
+        playerCards.appendChild(cardImgElement);
+    }
+};
+
+function displayOpponentCardsInHtml(opponentCards){
+    let i = 0;
+    for (i=0; i<2; i++) {
+        let cardImgElement = document.createElement('img');
+        cardImgElement.src= data.cards[i].image;
+        opponentCards.appendChild(cardImgElement);
+    }
+};
+
+drawPlayerCards()
     .then((response) => {
+        drawOpponentCards();
         console.log(deckId);
-        
 
 
 
@@ -49,3 +58,21 @@ drawSomeCards()
 
     })
 
+    function drawOpponentCards() {
+        return fetch(drawCards)
+            .then((response) => {
+                return response.json();
+            })
+                .then((data) => {
+                    deckId = data.deck_id;
+                    console.log(data)
+                
+                // function displayPlayerCardsInHtml(cards){
+                    let i = 0;
+                    for (i=0; i<2; i++) {
+                        let cardImgElement = document.createElement('img');
+                        cardImgElement.src= data.cards[i].image;
+                        opponentCards.appendChild(cardImgElement);
+                    }
+            });
+        };
