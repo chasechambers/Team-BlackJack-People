9b2f07b7239034467c691d90f08e544c892c12b7
Chase <chambers.chase@gmail.com>
2023-05-03 19:05:05 -0400

    updated javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
+// API VARIABLES
 let newDeckUrl = 'https://deckofcardsapi.com/api/deck/new/';
 let shuffleDeckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
-let newDeck = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2';
+let newDeck = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
 
 // DOM MANIPULATION VARIABLES
 let playerDeck = document.getElementById('player-deck');
@@ -8,9 +9,9 @@ let playerCards = document.getElementById('player-cards');
 let opponentCards = document.getElementById('oppo-cards');
 let hitMeButton = document.getElementById('hit');
 let stayMeButton = document.getElementById('stay');
-let stayMeButton = document.getElementById('stay');
 let playerScore = document.getElementById('player-hand-total');
-let dealerScore = document.getElementById('oppo-hand-total');
+
+//GLOBAL VARIABLES
 
 let playerScoreArray = [];
 let dealerScoreArray = [];
@@ -26,38 +27,40 @@ let valueMapper = {
     "7" : 7,
     "8" : 8,
     "9" : 9,
-    "10" : 10,
     "JACK" : 10,
     "QUEEN" : 10,
     "KING" : 10,
     "ACE" : 11,
 };
 
-function retrieveNewDeck() {
-    return fetch(newDeck)
-        .then((response) => {
-            return response.json();
-        })
-            .then((data) => {
-                deckId = data.deck_id;
-        });
-    };
-  
+// GET A NEW DECK AT BEGINNING OF THE GAME
 
-    function universalDrawCard(containerElement){
-        let drawCards = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
-       return fetch(drawCards)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            let cardImgElement = document.createElement('img');
-            cardImgElement.src  = data.cards[0].image;
-            cardImgElement.dataset.cardValue = valueMapper[data.cards[0].value];
-            containerElement.appendChild(cardImgElement);
-           
+function retrieveNewDeck() {
+return fetch(newDeck)
+    .then((response) => {
+        return response.json();
     })
-    };
+        .then((data) => {
+            deckId = data.deck_id;
+    });
+};
+
+// When called draws a card and gives it to either the player or the dealer
+
+function universalDrawCard(containerElement){
+    let drawCards = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+    fetch(drawCards)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        let cardImgElement = document.createElement('img');
+        cardImgElement.src= data.cards[0].image;
+        cardImgElement.dataset.cardValue = valueMapper[data.cards[0].value];
+        containerElement.appendChild(cardImgElement);
+    });
+};
+
 
 function buildArray(containerElement, array) {
     let i = 0;
@@ -78,6 +81,11 @@ function calculateScore(array, scoreContainer) {
     scoreContainer.textContent = sum;
 }
 
+
+
+
+//HIT ME BUTTON FUNCTION
+
 hitMeButton.addEventListener('click', function() {
     playerScoreArray.length = 0;
     universalDrawCard(playerCards);
@@ -90,9 +98,101 @@ hitMeButton.addEventListener('click', function() {
    
 });
 
+// STAY BUTTON FUNCTIONALITY
 
+stayMeButton.addEventListener('click', function() {
 
+        
+    if (dealerScore.textContent < 16 ) {
+        dealerScoreArray.length = 0;
+        universalDrawCard(opponentCards);
+        setTimeout(() => {
+            buildArray(opponentCards, dealerScoreArray);
+            calculateScore(dealerScoreArray, dealerScore)
+        }, 500);
+    } 
+    setTimeout(() => {
+        if (dealerScore.textContent < 16) {
+            console.log ('Need to draw again')
+            dealerScoreArray.length = 0;
+            universalDrawCard(opponentCards);
+            setTimeout(() => {
+                buildArray(opponentCards, dealerScoreArray);
+                calculateScore(dealerScoreArray, dealerScore)
+            }, 500);
+            
+        } else { console.log ('Stop drawing cards');
+            compliment
+        
+}
+    }, 500);
+});
 
+// creates an object with the user's score and initials
+
+function saveScore() {
+    var initials = prompt("Please enter your initals.")
+
+    var scoreData = {
+        initials: initials,
+        score: score
+    };
+    var scores = JSON.parse(localStorage.getItem("scores")) || [];
+
+    scores.push(scoreData);
+
+    localStorage.setItem('scores', JSON.stringify(scores));
+}
+
+// adds event listener to the submit button
+var submitButton = document.querySelector("#submit");
+if (submitButton) {
+  submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    saveScore();
+    window.location.replace("./highscores.html");
+  });
+}
+
+// checks is playerScore is equal to 21
+function checkWinner(playerScore, houseScore) {
+    if (playerScore === 21 && houseScore != 21){
+        score += 1;
+        console.log("you win!");
+        console.log(score);
+    }
+    else if (houseScore === 21 && playerScore != 21){
+        console.log("you lose!");
+        saveScore();
+        console.log(score);
+    }
+    else if (playerScore === 21 && houseScore === 21){
+        console.log("its a tie!");
+        console.log(score);
+    }
+    else if(playerScore > 21){
+        console.log("you lose!");
+        saveScore();
+        console.log(score);
+    }
+    else if(houseScore > 21){
+        score += 1;
+        console.log("you win!");
+        console.log(score);
+    }
+    else if(playerScore<= 21 && playerScore>houseScore){
+        score += 1;
+        console.log("you win!");
+        console.log(score);
+    }
+    else if(houseScore<= 21 && houseScore>playerScore){
+        console.log("you lose!");
+        saveScore();
+        console.log(score);
+    }
+};
+
+    // THIS IS THE CODE RUNNING
 
     retrieveNewDeck()
     .then(() => {
@@ -109,39 +209,34 @@ hitMeButton.addEventListener('click', function() {
            
     })
 
+var audio = new Audio('assets/audio/casino-music.mp3');
+var audioPlayed = false;
+var isMuted = false;
 
-    
-    stayMeButton.addEventListener('click', function() {
+var audioIcon = document.getElementById('audio-icon');
 
-        
-        if (dealerScore.textContent < 16 ) {
-            dealerScoreArray.length = 0;
-            universalDrawCard(opponentCards);
-            setTimeout(() => {
-                buildArray(opponentCards, dealerScoreArray);
-                calculateScore(dealerScoreArray, dealerScore)
-            }, 500);
-        } 
-        setTimeout(() => {
-            if (dealerScore.textContent < 16) {
-                console.log ('Need to draw again')
-                dealerScoreArray.length = 0;
-                universalDrawCard(opponentCards);
-                setTimeout(() => {
-                    buildArray(opponentCards, dealerScoreArray);
-                    calculateScore(dealerScoreArray, dealerScore)
-                }, 500);
-                
-            } else { console.log ('Stop drawing cards')
+// plays music when anything is clicked
+document.addEventListener('click', function () {
+    if (!audioPlayed) {
+      audio.volume = 0.2;
+      audio.play();
+      audioPlayed = true;
+      document.getElementById("stop-audio").innerHTML = "Mute";
     }
-        }, 500);
+  });
+  
+  // pauses music if pause button is pressed
+  document.getElementById('stop-audio').addEventListener('click', function() {
+    if (audio.muted) {
+      audio.muted = false;
+      isMuted = false;
+      document.getElementById("stop-audio").innerHTML = "Mute";
+    } else {
+      audio.muted = true;
+      isMuted = true;
+      document.getElementById("stop-audio").innerHTML = "Unmute";
+    }
+  });
+  
 
-    //     while (dealerScore.textContent < 16) {
-    //         setTimeout(() => {
-    //             dealerScoreArray.length = 0;
-    //             universalDrawCard(opponentCards);
-    //             buildArray(opponentCards,dealerScoreArray);
-    //             calculateScore(dealerScoreArray, dealerScore);
-    //     })
-    // }
-    });
+
